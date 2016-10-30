7bf3ec76d5eea7f68491d186dbc2f8cc37b8c5b7
luciferlee1996 <luciferlee1996@hotmail.com>
2016-10-30 23:40:41 +0800

    Calc: test regex for parsing a math expression done.

--- HW1/src/Calc.java
+++ HW1/src/Calc.java
@@ -1,9 +1,18 @@
 /**
  * Created by luc on 16-10-29.
  */
+import java.util.*;
+
 public class Calc {
 
     public static void main(String[] args){
-        System.out.println("Hello, world");
+        //System.out.println("Hello, world");
+        Scanner scan = new Scanner(System.in);
+        //System.out.println(scan.hasNextInt());
+        String test = "100 + 2 *(3 / 7)";
+        String arr[] = test.split("\\s|(?<=[\\+\\-\\*\\/%()])(?=[^\\s])|(?<=[^\\s])(?=[\\+\\-\\*\\/%()])");
+        for (String item: arr) {
+            System.out.println(item);
+        }
     }
 }
