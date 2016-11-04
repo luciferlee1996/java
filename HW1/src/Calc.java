a22f8c705c1a262a6f21d9b799707fc0fe95acd1
luciferlee1996 <luciferlee1996@hotmail.com>
2016-11-04 22:00:49 +0800

    donegit status!

--- HW1/src/Calc.java
+++ HW1/src/Calc.java
@@ -1,10 +1,7 @@
 /**
- * Created by luc on 16-10-29.
+ * Created by *** on 16-10-29.
  */
 
-// HERE!!
-// test case "1+2 * 3 - (-4 / 2)" didn't pass
-// remember to debug
 
 import java.util.*;
 
@@ -49,6 +46,12 @@ public class Calc {
             put("(/", false);
             put("(%", false);
 
+            put("+)", true);
+            put("-)", true);
+            put("*)", true);
+            put("/)", true);
+            put("%)", true);
+
             // "cur" is the very first operator
             put("+", false);
             put("-", false);
@@ -62,27 +65,35 @@ public class Calc {
         // separate numbers and operators
         exp_list = Arrays.asList(exp.split("\\s|(?<=[\\+\\-\\*\\/%()])(?=[^\\s])|(?<=[^\\s])(?=[\\+\\-\\*\\/%()])"));
     }
-    private double fetch_num(ListIterator<String> liter){
+    private String fetch_num_string(ListIterator<String> liter){
         // tested
         if(!liter.hasNext()){
             // exception
-            return 0;
+            System.out.println("fetch num error, stack is empty");
+            return "0";
         }
         String str = liter.next();
-        double res;
-        if(str.equals("-")){
-            res = -Double.parseDouble(liter.next());
+        String sign = new String();
+        if(str.equals("(")){
+            return str;
         }
-        else if(str.matches("\\d+(\\.\\d+)?")){
+        else if(str.equals("-")){
+            if(!liter.hasNext()){
+                // exception
+                System.out.println("except a number behind minus sign.");
+            }
+            sign = "-";
+            str = liter.next();
+        }
+        if(str.matches("\\d+(\\.\\d+)?")){
             // str is a number
-            res = Double.parseDouble(str);
+            return sign + str;
         }
         else {
             // exception
-            //System.out.println("fetch_num() gets an illegal string!");
-            res = 0;
+            System.out.println("fetch_num_str() gets an illegal string, your input is " + str);
+            return "0";
         }
-        return res;
     }
     private double single_operation(double a, double b, String op){
         switch(op){
@@ -98,6 +109,7 @@ public class Calc {
                 return a % b;
             default:
                 // exception
+                System.out.println("single operation error, your input is " + a + op + b);
                 return 0;
         }
     }
@@ -107,34 +119,49 @@ public class Calc {
         }
         else{
             //exception
+            System.out.println("map searching error, your input key is " + last + cur);
             return true;
         }
     }
     private double eval_till_ending_op(String cur_op, double a){
         do{
-            if(!op_stack.isEmpty()) break;
+            if(op_stack.isEmpty()) break;
             String last_op = op_stack.pop();
-            if(!(needs_to_op(last_op, cur_op) || cur_op.equals(""))){
+            if(cur_op.equals(")") && last_op.equals("("))
+                break;
+            else if(!(needs_to_op(last_op, cur_op) || cur_op.equals(""))){
                 op_stack.push(last_op);
                 break;
             }
             if(!num_stack.isEmpty()){
                 double top = num_stack.pop();
                 a = single_operation(top, a, last_op);
+                //cur_op = last_op;
             }else{
                 // exception
+                System.out.println("number of nums error.");
                 return 0;
             }
 
         }while(true);
+        if(!cur_op.equals(")"))
+            op_stack.push(cur_op);
+        return a;
     }
     private double evaluate(ListIterator<String> liter){
-        double res = 0;
-        String last_op = new String();
         while(liter.hasNext()){
-            double a = fetch_num(liter);
+            double a = 0;
+            String a_str = fetch_num_string(liter);
+            if(a_str.equals("(")){
+                op_stack.push(a_str);
+                continue;
+            }
+            else{
+                a = Double.parseDouble(a_str);
+            }
             if(!liter.hasNext()){
-                // HERE!! reaches the end of the expression
+                num_stack.push(eval_till_ending_op("", a));
+                break;
             }
             String op = liter.next();
             switch (op){
@@ -143,19 +170,22 @@ public class Calc {
                 case "*":
                 case "/":
                 case "%":
-                    // ordinary operator
+                case ")":
+                    num_stack.push(eval_till_ending_op(op, a));
                     break;
                 case "(":
                     break;
-                case ")":
-                    // ")"
-                    break;
                 default:
                     // exception
+                    System.out.println("operator error, your input is " + op);
                     return 0;
             }
         }
-        return res;
+        if(num_stack.size() != 1){
+            Double a = num_stack.pop();
+            num_stack.push(eval_till_ending_op("", a));
+        }
+        return(num_stack.pop());
     }
     public double calculate(String exp){
         parse(exp);
@@ -165,15 +195,21 @@ public class Calc {
 
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        Calc calculator = new Calc();
         do{
+            System.out.println("Please enter an mathematical expression,");
+            System.out.println("enter \"_END_\" means to terminate:");
+            Calc calculator = new Calc();
             String input = scanner.nextLine();
+            if(input.equals("")){
+                System.out.println(0.0);
+                continue;
+            }
             if(input.equals("_END_")){
                 // terminate command
                 System.out.println("Goodbye!");
                 break;
             }
-            System.out.println(calculator.calculate(input));
+            System.out.println("Answer: " + calculator.calculate(input));
         }while(true);
     }
 }
